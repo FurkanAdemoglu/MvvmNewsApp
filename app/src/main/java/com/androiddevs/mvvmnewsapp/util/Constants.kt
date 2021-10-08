7523f7d1fd86f144b3335f7ff35290518b5bf9f7
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-08 13:25:33 +0300

    Search Part Maded For the app

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY="413ecdcaa3bd456faaec43baa37cb0e0"
         const val BASE_URL="https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY=500L
     }
 }
